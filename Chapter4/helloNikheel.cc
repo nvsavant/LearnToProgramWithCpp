bd48e9e74a9b60717da6281119b89f3381655617
nvsavant <nvishwas@seas.upenn.edu>
2018-02-23 00:02:01 -0500

    Chapter 4

---
+++ Chapter4/helloNikheel.cc
@@ -0,0 +1,18 @@
+/*
+ * helloNikheel.cc
+ *
+ *  Created on: Feb 23, 2018
+ *      Author: nvishwas
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+	string firstName = "Nikheel";
+	cout << "Hello " << firstName << endl;
+
+	return 0;
+}
